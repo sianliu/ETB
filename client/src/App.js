7da6fa0eb2aefca8db5c1668005c6c791e4244c1
Jon Liu <mr@sianliu.com>
2022-05-05 18:18:21 +0800

    Connected frontend to smart contract using ethers

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-
+import getBlockchain from './ethereum.js';
+import React, {useEffect, useState } from 'react';
 
 function App() {
+  const [wallet, setWallet] = useState(undefined);
+
+  useEffect(() => {
+    const init = async () => {
+      const { wallet } = await getBlockchain();
+      setWallet(wallet);
+    };
+    init();
+  }, []);
+
+  if(
+    typeof wallet === 'undefined'
+  ) {
+    return 'Loading...';
+  }
+
   return (
     <div className="App">
-      Multisig Dapp 
     </div>
   );
 }
