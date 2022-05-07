757908978b20e152654285b28a6de49fd935e4f0
Jon Liu <mr@sianliu.com>
2022-05-07 18:33:24 +0800

    App.tsx - modified removed comment causing error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,7 +20,6 @@ function App() {
   }
 
   return (
-    // @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     <div className="App">
     </div>
   );
