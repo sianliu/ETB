adea1b2bb2018966287da924756f8c761a5a7dd4
Jon Liu <mr@sianliu.com>
2022-05-07 15:43:48 +0800

    [ts-migrate][.] Init tsconfig.json file
    Co-authored-by: ts-migrate <>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,7 +9,6 @@ root.render(
   // @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
   <React.StrictMode>
     // @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
-    <App />
   </React.StrictMode>
 );
 
